9f26d17dc7572fb5cc146e029593bbdc9699a9b2
olga liferenko <o.liferenko777@gmail.com>
2024-03-10 08:49:30 +0200

    green test for mobile devises

---
+++ test_guest_mobile.py
@@ -0,0 +1,9 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_has_title(page: Page):
+    page.goto("https://sigmath-stage.fly.dev/sessions/sess_02vjayEow5Iz22wIKXGJ6M")
+    page.get_by_label("Name").click()
+    page.get_by_label("Name").fill("Olga.test")
+    page.get_by_role("button", name="Submit").click()
+    expect(page.get_by_text("Rotate your device for best")).to_be_visible()
