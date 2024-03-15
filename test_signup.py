0975eddda44bf0ad02d60c843e1f9e77319c7195
olga liferenko <o.liferenko777@gmail.com>
2024-03-15 10:22:40 +0200

    желтій тест , генерирую email, использую для тестирования регистрации и авторизации

--- test_signup.py
+++ test_signup.py
@@ -2,8 +2,10 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 from random import choice
 from string import ascii_uppercase
+random_part = ''.join(choice(ascii_uppercase) for i in range(12))
+email = f'klara_{random_part}@test.com'
 
-def run(playwright: Playwright) -> None:
+def run_signup(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
@@ -13,11 +15,13 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("link", name="Sign up").click()
     page.get_by_label("Name").click()
     page.get_by_label("Name").fill("Klaaaaaaalllllllrrrrrrraaaaaaa")
+
     page.get_by_label("Email").click()
 
-    random_part = ''.join(choice(ascii_uppercase) for i in range(12))
+    #random_part = ''.join(choice(ascii_uppercase) for i in range(12))
 
-    email = f'klara_{random_part}@test.com'
+    #email = f'klara_{random_part}@test.com'
+    print(email)
 
     page.get_by_label("Email").fill(email)
     page.get_by_label("Password").click()
@@ -27,11 +31,30 @@ def run(playwright: Playwright) -> None:
     page.screenshot(path= f"screenshot_{email}.png")
     expect(page.get_by_role("link", name="Your profile")).to_be_visible()
 
+    #get_by_role("link", name="Log out")
+
+
     # ---------------------
     context.close()
     browser.close()
 
+def run_signin(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    #expect(page.get_by_role("link", name="Log out")).to_be_visible()
+    
+    page.goto("https://sigmath-stage.fly.dev/")
+    page.get_by_role("link", name="Log in").click()
+    page.get_by_label("Email").click()
+    page.get_by_label("Email").fill(email)
+    page.get_by_label("Password").click()
+    page.get_by_label("Password").fill("123456789")
+    page.get_by_label("Keep me logged in").check()
+    page.get_by_role("button", name="Sign in").click()
+
 
 with sync_playwright() as playwright:
-    run(playwright)
+    run_signup(playwright)
+    run_signin(playwright)
 
