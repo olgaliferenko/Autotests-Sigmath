f2f2c9ac3bcf5a7fede7c1d5cd1943cd47d9e2cf
olga liferenko <o.liferenko777@gmail.com>
2024-03-21 09:36:43 +0200

    каждый участник заходит с нового размера экрана

--- test_many_guests.py
+++ test_many_guests.py
@@ -11,17 +11,24 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 # - for every guest set new country
 
 def run(playwright: Playwright) -> None:
-    #browser = playwright.chromium.launch(proxy={"server": "http://myproxy.com"})
-    browser = playwright.firefox.launch(proxy={"server": "http://google.com"})
+    browser = playwright.chromium.launch(headless=False)
+
+    mobile_screen = {"width": 360, "height": 740}
+    desctop_screen = {"width": 1366, "height": 768}
+    desctop2_screen = {"width": 1920, "height": 1080}
+    screen_list = [mobile_screen, desctop_screen, mobile_screen, desctop2_screen, mobile_screen, desctop_screen,]
+    
+    #screen_list refactor
 
     context = browser.new_context()
 
     #user 1
-    list_of_numbers = range(1, 13, 1)
+    list_of_numbers = range(0, 4, 1)
 
     for index in list_of_numbers:
         username = f'user {index}'
         page = context.new_page()
+        page.set_viewport_size(screen_list[index])
         page.on("request", lambda request: print(">>", request.method, request.url))
         page.on("response", lambda response: print("<<", response.status, response.url))
         page.goto("https://sigmath-stage.fly.dev/sessions/sess_02vjayEow5Iz22wIKXGJ6M")
