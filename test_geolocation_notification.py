b7b2a304db0fe98de4db2fdb0c9767f74e689e13
olga liferenko <o.liferenko777@gmail.com>
2024-07-21 09:45:58 +0300

    remove geo test - we don't need it right now

--- test_geolocation_notification.py
+++ test_geolocation_notification.py
@@ -1,39 +1,40 @@
-import time
-from playwright.sync_api import Playwright, sync_playwright, expect
+# import time
+# from playwright.sync_api import Playwright, sync_playwright, expect
 
-#TODO :
-# - for every guest set new country
-# - check that RU sees notification
+# #TODO :
+# !!! This feature(фича) won't be added in Sigmath - so we dont need this test
+# # - for every guest set new country
+# # - check that RU sees notification
 
 
-def run(playwright: Playwright) -> None:
-    # find free proxy in Google and add it here
-    proxy_serbia = "217.26.67.57:3180"
-    proxy_ukraina = "31.43.158.108:8888"
-    proxy_ru = "82.146.45.136:3128"
+# def run(playwright: Playwright) -> None:
+#     # find free proxy in Google and add it here
+#     proxy_serbia = "217.26.67.57:3180"
+#     proxy_ukraina = "31.43.158.108:8888"
+#     proxy_ru = "82.146.45.136:3128"
 
-    browser = playwright.firefox.launch(proxy={"server": proxy_ru})
+#     browser = playwright.firefox.launch(proxy={"server": proxy_ru})
 
-    context = browser.new_context()
+#     context = browser.new_context()
 
-    #user 1
-    list_of_numbers = range(1, 13, 1)
+#     #user 1
+#     list_of_numbers = range(1, 13, 1)
 
-    for index in list_of_numbers:
-        username = f'user {index}'
-        page = context.new_page()
-        page.on("request", lambda request: print(">>", request.method, request.url))
-        page.on("response", lambda response: print("<<", response.status, response.url))
-        page.goto("https://sigmath-stage.fly.dev/sessions/sess_02vjayEow5Iz22wIKXGJ6M")
-        page.get_by_label("Name").click()
-        page.get_by_label("Name").fill(username)
-        page.get_by_role("button", name="Submit").click()
+#     for index in list_of_numbers:
+#         username = f'user {index}'
+#         page = context.new_page()
+#         page.on("request", lambda request: print(">>", request.method, request.url))
+#         page.on("response", lambda response: print("<<", response.status, response.url))
+#         page.goto("https://sigmath-stage.fly.dev/sessions/sess_02wJ6DT6jQb4jStuSH3EdL")
+#         page.get_by_label("Name").click()
+#         page.get_by_label("Name").fill(username)
+#         page.get_by_role("button", name="Submit").click()
 
-    # ---------------------
-    time.sleep(5)
-    context.close()
-    browser.close()
+#     # ---------------------
+#     time.sleep(5)
+#     context.close()
+#     browser.close()
    
 
-with sync_playwright() as playwright:
-    run(playwright)
+# with sync_playwright() as playwright:
+#     run(playwright)
